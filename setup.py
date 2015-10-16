0733809d14f3e5ca884058ed5cfb023fea4a0855
Stefan Midjich <swehack@gmail.com>
2015-10-16 11:46:23 +0200

    added entry_points configuration in plugins.cfg.

--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 from setuptools import setup, find_packages
 
-#plugins_path = './plugins'
-#packages = find_packages(plugins_path)
-
 setup(
     name="ECSPlugins",
     version="0.1",
     description="ECS Plugins",
     author="Stefan Midjich",
     packages=find_packages(),
-    entry_points="""
-    [ecs.plugins]
-    SamplePlugin = sample_plugin:SamplePlugin
-    LoggingPlugin = logging_plugin:LoggingPlugin
-    """
+    entry_points=open('plugins.cfg').read()
 )
