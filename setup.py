0f845dda15c48c97f14efec2c26d42aea7bacb3e
Stefan Midjich <swehack@gmail.com>
2015-10-27 11:17:33 +0100

    making use of global /etc/ecs_plugins.cfg if available.

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
+try:
+    plugins = open('/etc/ecs_plugins.cfg')
+except:
+    plugins = open('plugins.cfg')
+
 setup(
     name="ECSPlugins",
     version="0.1",
     description="ECS Plugins",
     author="Stefan Midjich",
     packages=find_packages(),
-    entry_points=open('plugins.cfg').read()
+    entry_points=plugins.read()
 )
